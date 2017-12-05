354c45d817d446d6891c7b9c11ebd13457c5394d
taoqf <tao_qiufeng@126.com>
2017-12-05 09:40:20 +0800

    add interfaces `setTopBarText`, `setNavigationBarColor`

--- index.d.ts
+++ index.d.ts
@@ -1527,6 +1527,31 @@ declare namespace wx {
 	 * 隐藏导航条加载动画。
 	 */
 	function hideNavigationBarLoading(): void;
+	type AnimationTimingFunc =
+		'linear'		// 动画从头到尾的速度是相同的
+		| 'easeIn'		// 动画以低速开始
+		| 'easeOut'		// 动画以低速结束。
+		| 'easeInOut'	// 动画以低速开始和结束。
+		;
+	interface SetNavigationBarColorOptions extends BaseOptions {
+		frontColor: string;	// 前景颜色值，包括按钮、标题、状态栏的颜色，仅支持 #ffffff 和 #000000
+		backgroundColor: string;	// 背景颜色值，有效值为十六进制颜色
+		animation?: Partial<{		// 动画效果
+			duration: number;					// 动画变化时间，默认0，单位：毫秒
+			timingFunc: AnimationTimingFunc;	// 动画变化方式，默认 linear
+		}>;
+		success?(res: {
+			errMsg: string;	// 调用结果
+		}): void;
+	}
+	function setNavigationBarColor(options: SetNavigationBarColorOptions): void;
+}
+
+declare namespace wx {
+	interface SetTopBarTextOptions extends BaseOptions {
+		text: string;	// 置顶栏文字内容
+	}
+	function setTopBarText(options: SetTopBarTextOptions): void;
 }
 
 // 界面-----导航
